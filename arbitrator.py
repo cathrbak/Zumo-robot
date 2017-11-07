1d9f8f0c157a499249ce738429a35e394a7b6c23
EliasKristoffer <eliasks@ntnu.no>
2017-11-07 12:14:47 +0100

    first

---
+++ arbitrator.py
@@ -0,0 +1,20 @@
+from bbcon import BBCON
+from random import random as random
+
+
+class Arbitrator:
+
+    def __init__(self):
+        self.bbcon = BBCON()
+
+    def choose_action(self):
+        # Henter inn behaviours, velger behaviour tilfeldig ut ifra hver weight
+        behaviors = self.bbcon.behaviors
+        total_weight = sum(b.weight for b in behaviors)
+        random_number = random(total_weight)
+
+        w = total_weight
+        for behavior in behaviors:
+            w -= behavior.weight
+            if random_number > w:
+                return behavior.motor_recommendations, behavior.halt_request

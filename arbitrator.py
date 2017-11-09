87ef41c81b7c5acfdcd0410d7fe46f4f73b44381
EliasKristoffer <eliasks@ntnu.no>
2017-11-09 10:41:51 +0100

    fix

--- arbitrator.py
+++ arbitrator.py
@@ -3,17 +3,27 @@ from random import random as random
 
 class Arbitrator:
 
-    def __init__(self):
-        pass
+    def __init__(self, randomchoice=False):
+        self.randomchoice=randomchoice
 
     def choose_action(self, behaviours):
-        # Velger behaviour tilfeldig ut ifra hver weight
+        if self.randomchoice:
+            # Velger behaviour tilfeldig ut ifra hver weight
 
-        total_weight = sum(b.weight for b in behaviours)
-        random_number = random()*total_weight
+            total_weight = sum(b.weight for b in behaviours)
+            random_number = random()*total_weight
 
-        w = total_weight
-        for behaviour in behaviours:
-            w -= behaviour.weight
-            if random_number >= w:
-                return behaviour.motor_recoms, behaviour.halt_request
+            w = total_weight
+            for behaviour in behaviours:
+                w -= behaviour.weight
+                if random_number >= w:
+                    return behaviour.motor_recoms, behaviour.halt_request
+        else:
+            hi = -1
+            temp = None
+            for behaviour in behaviours:
+                if behaviour.weight > hi:
+                    hi = behaviour.weight
+                    temp = behaviour
+            if temp:
+                return temp.motor_recoms, temp.halt_request

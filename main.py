0af07aee58de16065c80f97c7c76ad2af3c29c48
janinestang <helenejs@stud.ntnu.no>
2017-11-09 09:36:21 +0100

    heihei

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
 from bbcon import BBCON
 from behaviour import FollowLine
+from zumo_button import ZumoButton
 
 def main():
 
@@ -8,6 +8,8 @@ def main():
     follow_line = FollowLine(bbcon)
 
     bbcon.add_behaviour(follow_line)
+
+    ZumoButton().wait_for_press()
 
     while True:
         bbcon.run_one_timestep()
